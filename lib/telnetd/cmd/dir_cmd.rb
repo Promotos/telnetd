7d581c4915dd5ad66f978c22f0ddfb4b5225547a
Promotos <promotos@gmx.de>
2013-04-29 20:07:25 +0200

    show file size in a human readable format.

--- lib/telnetd/cmd/dir_cmd.rb
+++ lib/telnetd/cmd/dir_cmd.rb
@@ -45,9 +45,18 @@ private
 	def files(context)
 		result = ""
 		context.list_files.each { |file|
-			result +=  "\r\n\t%-40s \t %10d" % [File.basename(file), File.size(file)]
+			result +=  "\r\n\t%-40s \t %-10s" % [File.basename(file), as_size(File.size(file))]
 		}
 		return result		
 	end
+
+	def as_size(s)
+  		units = %W(B KiB MiB GiB TiB)
+  		size, unit = units.reduce(s.to_f) do |(fsize, _), utype|
+    			fsize > 512 ? [fsize / 1024, utype] : (break [fsize, utype])
+  		end
+  		"#{size > 9 || size.modulo(1) < 0.1 ? '%d' : '%.1f'} %s" % [size, unit]
+	end
+
 end
 end
